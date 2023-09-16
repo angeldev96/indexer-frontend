86b597cce3938f2924b20cc1d63157e7e4edb133
angelde96 <arivalladares2.0@gmail.com>
2023-09-16 14:00:48 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h-screen bg-gray-100 p-8">
+    <EmailSearch />
+  </div>
+</template>
+
+<script>
+import EmailSearch from './components/EmailSearch.vue';
+
+export default {
+  components: {
+    EmailSearch
+  }
+};
+</script>
---
+++ src/components/EmailSearch.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="p-4">
+      <div class="flex items-center">
+        <input 
+          v-model="keyword" 
+          placeholder="Buscar palabra clave..." 
+          class="p-2 border rounded mr-2 flex-grow"
+        />
+        <button @click="searchEmails" class="bg-blue-500 text-white p-2 rounded hover:bg-blue-600">
+          Search
+        </button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        keyword: ''
+      };
+    },
+    methods: {
+      searchEmails() {
+        // Aquí es donde harías la llamada a la API en el futuro
+        console.log("Buscando:", this.keyword);
+      }
+    }
+  };
+  </script>
+  
